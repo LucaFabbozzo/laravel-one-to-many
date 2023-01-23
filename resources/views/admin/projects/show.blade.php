eaee280e9073011f34b71063ca19e83c4293c6f4
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-23 11:24:50 -0500

    add bottom return to project type list

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="container w-75 m-auto text-center pt-2 text">
-        <a href="{{route('admin.projects.index')}}" class="btn btn-outline-secondary text-uppercase my-4">Back to the List</a>
+        <a href="{{route('admin.projects.index')}}" class="btn btn-outline-secondary text-uppercase my-4 me-2">Back to the List</a>
+        <a href="{{route('admin.types_project')}}" class="btn btn-outline-secondary text-uppercase my-4">Back to the Project Type List</a>
         <div class="card m-auto p-2 mb-5" style="width: 70%;">
          <img style="width: 40%" src="{{asset('storage/'. $project->cover_image)}}" class="card-img-top m-auto" alt="{{$project->image_original_name}}">
             <div class="card-body">
