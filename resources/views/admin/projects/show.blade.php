6420f3ae2559e57d47c99508d8829d17f54e0aa8
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-23 10:16:54 -0500

    add type_id into fillable Project Model

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,7 +4,7 @@
     <div class="container w-75 m-auto text-center pt-2 text">
         <a href="{{route('admin.projects.index')}}" class="btn btn-outline-secondary text-uppercase my-4">Back to the List</a>
         <div class="card m-auto p-2 mb-5" style="width: 70%;">
-         <img style="width: 40%" src="{{asset('storage/'. $project->cover_image)}}" class="card-img-top m-auto p-2 bg-dark" alt="{{$project->image_original_name}}">
+         <img style="width: 40%" src="{{asset('storage/'. $project->cover_image)}}" class="card-img-top m-auto" alt="{{$project->image_original_name}}">
             <div class="card-body">
                 <h4 class="card-title fs-2">{{$project->name}} <span class="badge text-bg-info">{{ $project->type->name }}</span></h4>
                 <p class="card-title fs-4">{{$project->client_name}}</p>
